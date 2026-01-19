b7da9ed5167fffab46c4d3501c5b33ef58f6a465
mfondev <ataubagideon@gmail.com>
2026-01-19 13:19:53 +0100

    Refactor tab layout and add new screens for Recipe, Saved, and Accounts

---
+++ app/(tabs)/accounts.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Accounts() {
+  return (
+    <View>
+      <Text>Acounts</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ app/(tabs)/recipe.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Recipe() {
+  return (
+    <View>
+      <Text>Recipe</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
